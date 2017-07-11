37785a9e43f8f03e59a8cf6c56610de92dcaec45
Matthew Victor <vicmcnabby@gmail.com>
2017-07-11 15:54:51 -0600

    added production

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/galvanizereads'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
